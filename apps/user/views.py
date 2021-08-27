8c4d4288999fa14aefc20490ea0e7850379ecd65
belecastillo <belencastillo2207@gmail.com>
2021-08-27 18:32:33 -0400

    add views

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
 # Create your views here.
+
+def inicio(request):#para toda peticion proveniente del navegador debe usarse request
+    return render(request,'inicio.html')
+
+
+def logout_Usuario(request):
+    logout(request)
+    return HttpResponseRedirect('http://localhost:8000/')

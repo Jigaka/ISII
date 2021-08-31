b10b172a695336439f3f3fc9d2cd0cb751a189ab
belecastillo <belencastillo2207@gmail.com>
2021-08-30 22:54:47 -0400

    add user urls

---
+++ apps/user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from apps.user.views import ListarUsuario, EliminarUsuario, ActualizaUsuario, ActivosUsuario
+
+urlpatterns = [
+    path('listar_usuario/', login_required(ListarUsuario.as_view(),login_url='login'), name='listar_usuario'),
+    path('listar_activos/',login_required(ActivosUsuario.as_view(),login_url='login'), name='listar_activos'),
+    path('actualizar_usuario/<int:pk>/',login_required(ActualizaUsuario.as_view(),login_url='login'), name = 'actualizar_usuario'),
+    path('eliminar_usuario/<int:pk>/',login_required(EliminarUsuario.as_view(),login_url='login'), name='eliminar_usuario'),
+]

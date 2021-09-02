e41985705e2f6d9f37d74c3052e15f1c87487eff
Roxana Fernández <roxana.jacquelinefa@gmail.com>
2021-09-02 01:19:29 -0400

    Se modificó el path 'crear_proyectos/' a 'crear_proyecto/' ya que este último es más acertado

--- apps/proyectos/urls.py
+++ apps/proyectos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import crearProyecto, listarProyectos, editarProyecto, eliminarProyecto
 
 urlpatterns = [
-    path('crear_proyecto/',crearProyecto, name='crear_proyectos'),
+    path('crear_proyecto/',crearProyecto, name='crear_proyecto'),
     path('listar_proyectos/',listarProyectos, name='listar_proyectos'),
     path('editar_proyectos/<int:id>',editarProyecto, name='editar_proyectos'),
     path('eliminar_proyectos/<int:id>',eliminarProyecto, name='eliminar_proyectos')

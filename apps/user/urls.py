b106f79cd0a7b4b81bfa315622b5640b91f8ffa2
belecastillo <belencastillo2207@gmail.com>
2021-09-02 22:53:43 -0400

    se agregó las rutas para listar y crear permisos

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from apps.user.views import ListarUsuario, EliminarUsuario, ActualizaUsuario, ActivosUsuario
+from apps.user.views import ListarUsuario, EliminarUsuario, ActualizaUsuario, ActivosUsuario, ListarPermisos, CrearPermisos
 
 urlpatterns = [
     path('listar_usuario/', login_required(ListarUsuario.as_view(),login_url='login'), name='listar_usuario'),
     path('listar_activos/',login_required(ActivosUsuario.as_view(),login_url='login'), name='listar_activos'),
     path('actualizar_usuario/<int:pk>/',login_required(ActualizaUsuario.as_view(),login_url='login'), name = 'actualizar_usuario'),
     path('eliminar_usuario/<int:pk>/',login_required(EliminarUsuario.as_view(),login_url='login'), name='eliminar_usuario'),
+    path('listar_permisos/',ListarPermisos.as_view(),name = 'listar_permisos'),
+    path('crear_permisos/',CrearPermisos.as_view(),name = 'crear_permisos'),
 ]

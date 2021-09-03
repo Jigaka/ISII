6047ddb0363f31cdfc05038e9ed7cb00c21ce826
belecastillo <belencastillo2207@gmail.com>
2021-09-02 22:52:28 -0400

    se agregó una clase PermsForm para los permisos

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import Permission
 from .models import User
 
 class UserForm(forms.ModelForm):
@@ -10,4 +11,14 @@ class UserForm(forms.ModelForm):
             'last_name': 'Apellidos del usuario',
             'email': 'correo del usuario',
             'is_active': 'Estado del usuario'
+        }
+
+class PermsForm(forms.ModelForm):
+    class Meta:
+        model = Permission
+        fields = ['codename', 'name', 'content_type']
+        labels = {
+            'codename': 'codename',
+            'name': 'Nombre del permiso',
+            'content_type': 'Tipo de contenido'
         }

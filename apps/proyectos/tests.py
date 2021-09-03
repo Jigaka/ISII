653ccd543d82218ac60b18a8ab0adda02e9078e4
Jigaka <garcetejoseka@gmail.com>
2021-09-03 16:05:31 -0400

    add test for user, login and projects

--- apps/proyectos/tests.py
+++ apps/proyectos/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
 
-# Create your tests here.
+import pytest
+from django.test import TestCase
+from faker import Faker
+from ddf import G, F
+from apps.user.models import User
+from apps.proyectos.models import Proyec
+from apps.proyectos.forms import ProyectoForm
+
+fake = Faker()
+class ProyectModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Proyec.objects.create(nombre='tienda ')
+#se compueba los campos de la clase.
+    def test_first_name_label(self):
+        proyecto = Proyec.objects.get(id=1)
+        field_label = proyecto._meta.get_field('nombre').verbose_name
+        self.assertEquals(field_label, 'nombre')
+    def test_estado(self):
+        proyecto = Proyec.objects.get(id=1)
+        field_label = proyecto._meta.get_field('estado').verbose_name
+        self.assertEquals(field_label, 'estado')
+#necesitamos probar nuestros métodos personalizados, el siguiente solo verifica que el nombre del objeto se construyó como esperábamos usando el formato "nombre"
+    def test_object_nombre(self):
+        proyecto = Proyec.objects.get(id=1)
+        expected_object_name = '%s' % (proyecto.nombre)
+        self.assertEquals(expected_object_name, str(proyecto))
+# con ddf se generan objetos para luego comprobar la correcta creacion, tambien se comprueba la relacion ManytoMany entre los usuarios y el proyecto
+    def test_create_Proyec(self):
+        proyecto1 = G(Proyec)
+        assert proyecto1
+    @pytest.mark.django_db
+    #se prueba el form de proyectos
+    def test_forms_fail(self):
+        with pytest.raises(Exception):
+            form_data = {'nombre': 'laadfdlf'}
+            form = ProyectoForm(data=form_data)
+            self.assertTrue(form.is_valid())
+
+
+
+
+
+
--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+import pytest
+import factory
+from faker import Faker
+from django.test import TestCase, Client
+from apps.user.models import User
+from apps.user.forms import UserForm
+from ddf import G, F
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    #con ddf se prueba si en verdad se crea un objeto user
+    def test_create_User(self):
+        usuario=G(User)
+        assert usuario
+    # se pueba los campos de la clase.
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        User.objects.create(first_name='Jose', last_name='Garcete', username='big8', email="afasdfasd@gmail.com")
+
+    def test_first_name_label(self):
+        author = User.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label, 'nombre')
+
+    def test_email_label(self):
+        usuario= User.objects.get(id=1)
+        field_label = usuario._meta.get_field('email').verbose_name
+        self.assertEquals(field_label, 'dirección de correo electrónico')
+
+    #@pytest.mark.django_db

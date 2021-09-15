b4d3041c93f8c7243e28167edb10b4ec017f0fc5
Roxana Fernández <roxana.jacquelinefa@gmail.com>
2021-09-15 00:33:06 -0400

    Se añadió Sprint al admin.py

--- apps/proyectos/admin.py
+++ apps/proyectos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Proyec, RolProyecto
+from .models import Proyec, RolProyecto, Sprint
 
 
 class RolProyectoAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ class RolProyectoAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Proyec)
 admin.site.register(RolProyecto,RolProyectoAdmin)
+admin.site.register(Sprint)

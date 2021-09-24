32b520e735c30a1534b906e8023260fdbae08581
belecastillo <belencastillo2207@gmail.com>
2021-09-24 11:20:18 -0400

    se añadieron nuevas rutas

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from apps.user.views import ListarUsuario, EliminarUsuario, \
     ActualizaUsuario, ActivosUsuario, ListarPermisos, \
-    CrearPermisos, ListarRoles, CrearRoles, AgregarPermisosAlRol, AsignarRolUserProyecto
+    CrearPermisos, ListarRoles, CrearRoles, AgregarPermisosAlRol, AsignarRolUserProyecto, EliminarRol
 
 urlpatterns = [
     path('listar_usuario/', login_required(ListarUsuario.as_view(), login_url='login'), name='listar_usuario'),
@@ -13,6 +13,7 @@ urlpatterns = [
     path('crear_permisos/', CrearPermisos.as_view(), name='crear_permisos'),
     path('listar_roles/<int:pk>', ListarRoles.as_view(), name='listar_roles'),
     path('crear_rol/<int:pk>', CrearRoles.as_view(), name='crear_rol'),
+    path('eliminar_rol/<int:pk>', EliminarRol.as_view(), name='eliminar_rol'),
     path('asignar_rol/<int:pk>/<int:pl>', AsignarRolUserProyecto.as_view(), name='asignar_rol'),
     path('agregar_permisos_roles/<int:pk>/', AgregarPermisosAlRol.as_view(), name='agregar_permisos_roles'),
 ]

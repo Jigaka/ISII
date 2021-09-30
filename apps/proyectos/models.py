d12eb48823476214d037df5844a3620bf862378f
Jigaka <garcetejoseka@gmail.com>
2021-09-30 15:51:04 -0400

    se arregla modelo duplicado de historiadeusuario

--- apps/proyectos/models.py
+++ apps/proyectos/models.py
@@ -58,77 +58,7 @@ class Proyec(models.Model):
         encargado_del_proyecto = self.encargado.all().values_list('username', flat=True)
         return encargado_del_proyecto
 
-class HistoriaUsuario(models.Model):
-    Pendiente = 'Pendiente'
-    ToDo = 'ToDo'
-    Doing = 'Doing'
-    Done = 'Done'
-    QA = 'QA'
-    STATUS_CHOICES = (
-        (Pendiente, "Pendiente"),
-        (ToDo, 'ToDo'),
-        (Doing, 'Doing'),
-        (Done, 'Done'),
-        (QA, 'QA')
-    )
 
-    Baja = 'Baja'
-    Media = 'Media'
-    Alta = 'Alta'
-    Prioridad_CHOICES = (
-        (Baja, 'Baja'),
-        (Media, 'Media'),
-        (Alta, 'Alta'),
-    )
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=200, blank=False, null=False)
-    asignacion = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=True, related_name="asignacion")
-    descripcion = models.TextField(blank=False, null=False)
-    estado = models.CharField(max_length=15, choices=STATUS_CHOICES, default=1)
-    fecha = models.DateField("fecha", auto_now=True, auto_now_add=False)
-    estimacion = models.PositiveIntegerField(editable=True, default=0)
-    fecha_creacion = models.DateField("fecha cre", auto_now=False, auto_now_add=True, blank=True, null=True)
-    fecha_ToDo = models.DateField(blank=True, null=True)
-    fecha_Doing = models.DateField(blank=True,null=True)
-    fecha_Done = models.DateField(blank=True,null=True)
-    fecha_QA = models.DateField(blank=True, null=True)
-    estado_anterior = models.CharField(max_length=200, default='Pendiente')
-    prioridad = models.CharField(max_length=15, choices=Prioridad_CHOICES, default=1)
-    prioridad_numerica = models.IntegerField(null=False, default=1)
-    proyecto = models.ForeignKey(Proyec, on_delete=models.CASCADE, blank=True, null=True, related_name="proyecto")
-    aprobado_PB=models.BooleanField(default=False)
-    sprint_backlog=models.BooleanField(default=False)
-    estimacion_user=models.PositiveIntegerField(editable=True, default=0)
-    estimacion_scrum = models.PositiveIntegerField(editable=True, default=0)
-
-
-    def save(self, *args, **kwargs): # redefinicion del metodo save() que contiene nuestro trigger
-        # Aqui ponemos el codigo del trigger -------
-        if (self.prioridad=='Baja'):
-            self.prioridad_numerica=1
-        elif (self.prioridad=='Media'):
-            self.prioridad_numerica=2
-        elif (self.prioridad=='Alta'):
-            self.prioridad_numerica=3
-        super(HistoriaUsuario,self).save(*args,**kwargs)
-        # fin de trigger ------
-
-    class Meta:
-        verbose_name = 'Historia de Usuario'
-        verbose_name_plural = 'Historias de Usuario'
-
-    def __str__(self):
-        return self.nombre
-
-    def is_upperclass(self):
-        return self.prioridad in {self.Baja, self.Media, self.Alta}
-
-    def is_upperclasss(self):
-        return self.estado in {self.Pendiente, self.ToDo, self.Doing, self.Done, self.QA}
-
-    def obtener_asignacion(self):
-        asignacion_user = self.asignacion.all().values_list('username', flat=True)
-        return asignacion_user
 
 class RolProyecto(models.Model):
     '''

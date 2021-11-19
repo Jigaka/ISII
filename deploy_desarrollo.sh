ebb3ad790b17fb5743c4ff0481b5ce03bbbdbd7d
Jigaka <garcetejoseka@gmail.com>
2021-11-19 12:56:19 -0300

    add deploy para desarrollo

--- deploy_desarrollo.sh
+++ deploy_desarrollo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cd '..'
+source 'bin/activate'
+cd 'ISII'
 git pull
 # $1 el nombre de la rama a probar el tag
 # $2 el nombre del tag
 git checkout -b $1 $2
 sh poblacion_dev.sh postgres
-python manage.py runserver
+python3 manage.py runserver
